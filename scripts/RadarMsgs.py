e8c4874faac7ba94e343e8bf1b34eea340a693fe
avavav <avavav@gmail.com>
2015-10-08 20:26:13 +0800

    well progress, but still need to test; jit works, using more efficient parser so 20Hz with three devices ok, modified test and .yaml conf for 3 devices testing processing load

--- scripts/RadarMsgs.py
+++ scripts/RadarMsgs.py
@@ -1,6 +1,10 @@
 import math
 from foobar.msg import Esr_track
-
+from numba import jit, njit
+import numba
+from bitarray import bitarray
+from array import array
+import numpy as np
 
 class BitVector:
     def __init__(self,val):
@@ -20,7 +24,7 @@ class BitVector:
     def __getslice__(self,highIndx,lowIndx):
 	return (self._val>>lowIndx)&(2L**(highIndx-lowIndx+1)-1)
 
-def convertRadarToBitArray(msg):
+def convertRadarToBitVector(msg):
     b = BitVector(0)
     bit_index = 0
     msg_bytearray = bytearray(msg.data)
@@ -33,6 +37,18 @@ def convertRadarToBitArray(msg):
 	index = index + 1
     return b
 
+# @jit
+def convertRadarToArray(msg):
+    # b = array('B', 8*[0])
+    b = np.array(bytearray(msg.data), dtype=np.uint8)
+    # print b.view()
+    # msg_bytearray = bytearray(msg.data)
+    # index = 0
+    # for i in msg.data:
+	    # b[index] = msg_bytearray[index]
+	    # index = index + 1
+    return b
+
 
 def isSignalSignedType(signal):
     if signal._valuetype == '+':
@@ -40,15 +56,23 @@ def isSignalSignedType(signal):
     else:
 	return True
 
+@njit(numba.i8(numba.u8,numba.u1))
 def twosComplement(number, signalsize):
     return (number - (1 << signalsize))
 
-def getSignalNumber(barray, start_bit, signalsize, isByteorderIntel, isValuetypeiSigned, factor, offset):
+@njit(numba.u1(numba.u1))
+def getArrayIdxFromStartBit(n):
+    return (0 if (n+1)%8 == 0 else 8-((n+1)%8))  + (n/8)*8
+
+# @jit
+def getSignalNumber(barray_unpacked, barray, start_bit, signalsize, isByteorderIntel, isValuetypeiSigned, factor, offset):
     # just guard againts unhandled (yet) intel bytecode
     # motorola only for now
     if 1 == isByteorderIntel: raise UserWarning
 
-    barray_msb = barray[start_bit:start_bit]
+    # barray_msb = barray[start_bit:start_bit]
+    start_bit_idx = getArrayIdxFromStartBit(start_bit)
+    barray_msb = barray_unpacked[start_bit_idx]
 
     start_field_count = (start_bit+1) % 8
     if start_field_count == 0 : start_field_count = 8
@@ -56,52 +80,192 @@ def getSignalNumber(barray, start_bit, signalsize, isByteorderIntel, isValuetype
     factor_number = int(factor) if float(factor).is_integer() else float(factor)
     offset_number = int(offset) if float(offset).is_integer() else float(offset)
 
-    # same field end
-    if signalsize <= start_field_count:
-	signal_number = barray[start_bit:start_bit-signalsize+1]
-	# print type(factor), type(offset)
-	# return signal_number
-    else:
-	barray_map = BitVector(0)
-	end_field_count = (signalsize - start_field_count) % 8
-	field_count     = int(math.floor((signalsize - start_field_count) / 8))
-	end_bit_first_field =int( math.floor(start_bit/8)*8)
-	#first field
-	# print signalsize-1,signalsize-start_field_count,start_bit,end_bit_first_field
-	barray_map[signalsize-1:signalsize-start_field_count] =( barray[start_bit:end_bit_first_field])
-	#intermediary field(s):
-	running_index = end_bit_first_field + 8
-	running_index_map  = signalsize - start_field_count - 1
-	for i in range(0,field_count):
-	    barray_map[running_index_map:running_index_map-7] =( barray[running_index+7:running_index])
-	    running_index = running_index + 8
-	    running_index_map  = running_index_map - 8
-	#last field
-	# check if actually all fields already taken
-	if running_index < 57 and running_index_map >= 0:
-	    barray_map[end_field_count-1:0] = barray[running_index+7:running_index+8-end_field_count]
+    signal_number_bits = barray_unpacked[start_bit_idx:start_bit_idx+signalsize]
 
-	signal_number = barray_map[signalsize-1:0]
+    no_of_padding = signalsize % 8
+
+    signal_number_bits = np.concatenate((np.array(no_of_padding*[0],dtype=np.uint8), signal_number_bits))
+
+    signal_number = np.packbits(signal_number_bits)
+
+    if len(signal_number) < 8:
+	signal_number = np.concatenate((signal_number, np.array((8-len(signal_number))*[0],dtype=np.uint8)))
+	
+    signal_number = signal_number.view(np.uint64).tolist()[0]
+
+    # same field end
+    # if signalsize <= start_field_count:
+	# signal_number = barray[start_bit:start_bit-signalsize+1]
+	# # print type(factor), type(offset)
+	# # return signal_number
+    # else:
+	# barray_map = BitVector(0)
+	# end_field_count = (signalsize - start_field_count) % 8
+	# field_count     = int(math.floor((signalsize - start_field_count) / 8))
+	# end_bit_first_field =int( math.floor(start_bit/8)*8)
+	# #first field
+	# # print signalsize-1,signalsize-start_field_count,start_bit,end_bit_first_field
+	# barray_map[signalsize-1:signalsize-start_field_count] =( barray[start_bit:end_bit_first_field])
+	# #intermediary field(s):
+	# running_index = end_bit_first_field + 8
+	# running_index_map  = signalsize - start_field_count - 1
+	# for i in range(0,field_count):
+	    # barray_map[running_index_map:running_index_map-7] =( barray[running_index+7:running_index])
+	    # running_index = running_index + 8
+	    # running_index_map  = running_index_map - 8
+	# #last field
+	# # check if actually all fields already taken
+	# if running_index < 57 and running_index_map >= 0:
+	    # barray_map[end_field_count-1:0] = barray[running_index+7:running_index+8-end_field_count]
+
+
+	# signal_number = barray_map[signalsize-1:0]
 
 
     if isValuetypeiSigned and barray_msb: 
 	signal_number = twosComplement(signal_number, signalsize)
     return signal_number*factor_number+offset_number
 
-def crack(msg, signal_obj, signal_name):
-    barray = convertRadarToBitArray(msg)
-    # signal = self.test_signal
+
+# big endian assumption 
+@njit(numba.u8(numba.u1[:],numba.u1,numba.u1))
+def getBigEndiNumberFromBitNpArr(blist, idx, size):
+    signal_number = 0
+    for i in range (0,size):
+	signal_number = signal_number | (blist[idx+i] << (size - i))
+    return signal_number
+    
+
+# @jit
+# @profile
+def parseSignal(barray, barray_unpacked, signal_names, signals):
     signal_list ={} 
-    for signal in signal_obj:
+
+    #create uint64 from barray
+    # msg_data_number = 0
+    # for i in range (0,64):
+	# msg_data_number = msg_data_number | (barray_unpacked[i] << (i))
+
+    # print  msg_data_number, type(msg_data_number)
+    # print twosComplement(0xFFFFFFFF, 32), type(twosComplement(0xFFFFFFFF, 32))
+    # print twosComplement(0xFFFFFFFFFFFFFFFF, 64), type(twosComplement(0xFFFFFFFFFFFFFFFF, 64))
+    # print twosComplement(0x7FFFFFFF, 32), type(twosComplement(0x7FFFFFF, 32))
+    # print twosComplement(0x7FFFFFFFFFFFFFFF, 64), type(twosComplement(0x7FFFFFFFFFFFFFFF, 64))
+
+    for signal in signals:
+	start_bit_idx = getArrayIdxFromStartBit(signal._startbit)
+	signal_number = getBigEndiNumberFromBitNpArr(barray_unpacked, start_bit_idx, signal._signalsize)
+	# print signal_number, type(signal_number), barray_unpacked, type(barray_unpacked)
+
+	if isSignalSignedType(signal) and barray_unpacked.tolist()[start_bit_idx]:
+	    signal_number = twosComplement(signal_number, signal._signalsize)
+
+	signal_list[signal._name] = signal_number*signal._factor + signal._offset
+	
+    
+    # for signal in signal_names:
+    # for signal in signals:
+	# signal_number = 0
+	# start_bit = signal._startbit
+	# signalsize = signal._signalsize
+	# start_bit_idx = getArrayIdxFromStartBit(start_bit)
+	# barray_msb = barray_unpacked[start_bit_idx]
+
+	# start_field_count = (start_bit+1) % 8
+	# if start_field_count == 0 : start_field_count = 8
+
+	# signal_number_bits = barray_unpacked[start_bit_idx:start_bit_idx+signalsize]
+
+	# no_of_padding = signalsize % 8
+
+	# signal_number_bits = np.concatenate((np.array(no_of_padding*[0],dtype=np.uint8), signal_number_bits))
+
+	# signal_number = np.packbits(signal_number_bits)
+
+	# if len(signal_number) < 8:
+	    # signal_number = np.concatenate((signal_number, np.array((8-len(signal_number))*[0],dtype=np.uint8)))
+
+	# signal_number = signal_number.view(np.uint64).tolist()[0]
+
+	# signal_list[signal._name] = signal_number
+
+    return signal_list
+    # return None
+
+# @jit
+# @profile
+def parseSignal_new(barray, barray_unpacked, signal_names, signals):
+    signal_list ={} 
+    # for signal in signal_names:
+    for signal in signals:
+	signal_number = 0
 	start_bit = signal._startbit
-	# signal_number = barray[start_bit:start_bit]
-	signal_number = getSignalNumber(barray, start_bit, signal._signalsize, signal._byteorder, isSignalSignedType(signal), signal._factor, signal._offset)
-	signal_value = signal._values.get(signal_number,None)
-	# print signal._name, signal_number, isSignalSignedType(self.test_signal), signal_value, signal._offset, signal._factor
+	signalsize = signal._signalsize
+	start_bit_idx = getArrayIdxFromStartBit(start_bit)
+	barray_msb = barray_unpacked[start_bit_idx]
+
+	start_field_count = (start_bit+1) % 8
+	if start_field_count == 0 : start_field_count = 8
+
+	signal_number_bits = barray_unpacked[start_bit_idx:start_bit_idx+signalsize]
+
+	no_of_padding = signalsize % 8
+
+	signal_number_bits = np.concatenate((np.array(no_of_padding*[0],dtype=np.uint8), signal_number_bits))
+
+	signal_number = np.packbits(signal_number_bits)
+
+	if len(signal_number) < 8:
+	    signal_number = np.concatenate((signal_number, np.array((8-len(signal_number))*[0],dtype=np.uint8)))
+
+	signal_number = signal_number.view(np.uint64).tolist()[0]
+	
 	signal_list[signal._name] = signal_number
+
+    return signal_list
+
+
+def parseSignal_old(barray, barray_unpacked, signal_names):
+    signal_list ={} 
+    # return None
+    for signal in signal_names:
+    # for signal in signals:
+	# start_bit = signal._startbit
+	# signal_number = barray[start_bit:start_bit]
+	# signal_number = getSignalNumber(barray_unpacked, barray, start_bit, signal._signalsize, signal._byteorder, isSignalSignedType(signal), signal._factor, signal._offset)
+	signal_number = 0
+	# signal_value = signal._values.get(signal_number,None)
+	# print signal._name, signal_number, isSignalSignedType(self.test_signal), signal_value, signal._offset, signal._factor
+	# signal_list[signal._name] = signal_number
+	signal_list[signal] = signal_number
+
+    # print 'hi from parseSignal'
+    return signal_list
+
+
+# @jit(nopython=True)
+# @profile
+# @jit
+def crack(msg_data, signal_names, frame_name, signals):
+    # barray = convertRadarToBitVector(msg)
+    # barray = convertRadarToArray(msg)
+    # barray = np.array(bytearray(msg_data), dtype=np.uint8).tolist()
+    barray = np.array((msg_data), dtype=np.uint8)
+    # barray_unpacked  = np.unpackbits(barray).tolist()
+    barray_unpacked  = np.unpackbits(barray)
+    # signal = self.test_signal
+
+    # return None
+
+    signal_list = parseSignal(barray.tolist(), barray_unpacked, signal_names, signals)
+    return signal_list
+
     # try:
     # print signal_list
-    result_dict =  decode_dict.get(signal_name, None)
+
+@jit
+def decodeMsg(msg, signal_list, frame_name):
+    result_dict =  decode_dict.get(frame_name, None)
     if result_dict is not None:
 	return result_dict(signal_list, msg)
     else:
@@ -123,6 +287,8 @@ def decodeEsrTrack(signal_dict, msg):
     msg_pub.angle             = signal_dict['CAN_TX_TRACK_ANGLE']
     msg_pub.header.stamp = msg.header.stamp
     msg_pub.header.frame_id = msg.header.frame_id
+
+    # print msg_pub
     
     return msg_pub
 
