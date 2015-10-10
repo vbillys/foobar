fdc5589d22a61c6b66c88765e5f4bad50d9505d2
avavav <avavav@gmail.com>
2015-10-10 10:42:09 +0800

    added test send vehicle data and 4 devices, OK, tested!

--- scripts/RadarSync.py
+++ scripts/RadarSync.py
@@ -1,11 +1,16 @@
 import threading, Queue
+import rospy
 
+from foobar.msg import Canbus as CanBusMsg
+
+canbus_msg = CanBusMsg()
 
 class RadarEsrSyncThread(threading.Thread):
     def __init__(self, pub_can_send, sync_queue):
 	self.pub_can_send = pub_can_send
 	self.thread_stop = False
 	self.sync_queue  = sync_queue
+	self.sync_counter = 0
 	threading.Thread.__init__ (self)
    
     def setThreadStop(self, thread_stop):
@@ -13,13 +18,32 @@ class RadarEsrSyncThread(threading.Thread):
 
 
     def run(self):
+
+	rate = rospy.Rate(20)
+	canbus_msg.dlc = 8
+	canbus_msg.data = 8 * [0]
+	counter = 0
+
 	while not self.thread_stop:
 	    try:
-		val = self.sync_queue.get(True,1)
+		val = self.sync_queue.get(True,0.02)
 		self.sync_queue.task_done()
 	    except Queue.Empty:
-		print 'queue empty from radar_esr sync...'
+		# print 'queue empty from radar_esr sync...'
 		pass
+	    if counter == 2:
+		for cid in range(0x4f0, 0x4f2):
+		    canbus_msg.id = cid
+		    self.pub_can_send.publish(canbus_msg)
+	    elif counter == 5:
+		for cid in range(0x5f2, 0x5f6):
+		    canbus_msg.id = cid
+		    self.pub_can_send.publish(canbus_msg)
+
+	    counter = counter + 1
+	    self.sync_counter = self.sync_counter + 1
+	    if counter >= 6:
+		counter = 0
 
     def processEgomotion(self, egomotion_msg):
 	self.egomotion = egomotion_msg
--- scripts/can_test_msg.py
+++ scripts/can_test_msg.py
@@ -25,6 +25,7 @@ def startRosNode():
     pub_can_msg  = rospy.Publisher('radar_packet/can0/recv', CanBusMsg, queue_size=100)
     pub_can_msg2 = rospy.Publisher('radar_packet/can1/recv', CanBusMsg, queue_size=100)
     pub_can_msg3 = rospy.Publisher('radar_packet/can2/recv', CanBusMsg, queue_size=100)
+    pub_can_msg4 = rospy.Publisher('radar_packet/can3/recv', CanBusMsg, queue_size=100)
     canbus_msg.header.frame_id = 'front_middle'
     #canbus_msg.header.seq = 1
     canbus_msg.id = 0x053f # 0x17be
@@ -34,13 +35,13 @@ def startRosNode():
 
     barray = np.array(canbus_msg.data, dtype=np.uint8)
     barray_unpacked = np.unpackbits(barray)
-    setBigEndiNumberToNpArr(barray_unpacked, getArrayIdxFromStartBit(55), 8, 255)
+    setBigEndiNumberToNpArr(barray_unpacked, getArrayIdxFromStartBit(55), 8, 1)
     canbus_msg.data = np.packbits(barray_unpacked).tolist()
 
     #print len(canbus_msg.data)
 
     # rate = rospy.Rate(6000)
-    rate = rospy.Rate(40)
+    rate = rospy.Rate(20)
     while not rospy.is_shutdown():
         canbus_msg.header.stamp = rospy.Time.now()
 	# pub_can_msg.publish(canbus_msg)
@@ -48,28 +49,33 @@ def startRosNode():
 	for id in range(0x0500,0x0540):
 	    canbus_msg.id = id
 	    pub_can_msg.publish(canbus_msg)
-	    # pub_can_msg2.publish(canbus_msg)
-	    # pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg2.publish(canbus_msg)
+	    pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg4.publish(canbus_msg)
 	canbus_msg.id = 0x540
 	for i in range (0,10):
 	    pub_can_msg.publish(canbus_msg)
-	    # pub_can_msg2.publish(canbus_msg)
-	    # pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg2.publish(canbus_msg)
+	    pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg4.publish(canbus_msg)
 	for id in range(0x05d0,0x05d2):
 	    canbus_msg.id = id
 	    pub_can_msg.publish(canbus_msg)
-	    # pub_can_msg2.publish(canbus_msg)
-	    # pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg2.publish(canbus_msg)
+	    pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg4.publish(canbus_msg)
 	for id in range(0x05e4,0x05e9):
 	    canbus_msg.id = id
 	    pub_can_msg.publish(canbus_msg)
-	    # pub_can_msg2.publish(canbus_msg)
-	    # pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg2.publish(canbus_msg)
+	    pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg4.publish(canbus_msg)
 	for id in range(0x04e0,0x04e4):
 	    canbus_msg.id = id
 	    pub_can_msg.publish(canbus_msg)
-	    # pub_can_msg2.publish(canbus_msg)
-	    # pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg2.publish(canbus_msg)
+	    pub_can_msg3.publish(canbus_msg)
+	    pub_can_msg4.publish(canbus_msg)
 	    
         rate.sleep()
 
