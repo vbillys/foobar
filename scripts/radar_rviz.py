6f6bd9133360e4e54c46809f3fae690f3acfe9e8
avavav <avavav@gmail.com>
2015-10-29 11:54:07 +0800

    improved gui, segregated object target processing

--- scripts/radar_rviz.py
+++ scripts/radar_rviz.py
@@ -9,6 +9,72 @@ from std_msgs.msg import ColorRGBA
 from geometry_msgs.msg import Point
 from foobar.msg import GenericObjCanData
 
+def extractTrackingFromData(data):
+    _movingflags  = []
+    _powernumbers = []
+    _moveablefast = []
+    _moveableslow = []
+    for i in range(854+18,3440-258,258):
+	_movingflags.append(data.data[i])
+	_movingflags.append(data.data[i+1])
+	_movingflags.append(data.data[i+2])
+	_movingflags.append(data.data[i+3])
+	_movingflags.append(data.data[i+4])
+	_movingflags.append(data.data[i+5])
+	_movingflags.append(data.data[i+6])
+	_powernumbers.append(data.data[i  -7])
+	_powernumbers.append(data.data[i+1-7])
+	_powernumbers.append(data.data[i+2-7])
+	_powernumbers.append(data.data[i+3-7])
+	_powernumbers.append(data.data[i+4-7])
+	_powernumbers.append(data.data[i+5-7])
+	_powernumbers.append(data.data[i+6-7])
+	_moveableslow.append(data.data[i  +14])
+	_moveableslow.append(data.data[i+1+14])
+	_moveableslow.append(data.data[i+2+14])
+	_moveableslow.append(data.data[i+3+14])
+	_moveableslow.append(data.data[i+4+14])
+	_moveableslow.append(data.data[i+5+14])
+	_moveableslow.append(data.data[i+6+14])
+	_moveablefast.append(data.data[i  +7])
+	_moveablefast.append(data.data[i+1+7])
+	_moveablefast.append(data.data[i+2+7])
+	_moveablefast.append(data.data[i+3+7])
+	_moveablefast.append(data.data[i+4+7])
+	_moveablefast.append(data.data[i+5+7])
+	_moveablefast.append(data.data[i+6+7])
+    _movingflags.append(data.data[3440-258+18])
+    _powernumbers.append(data.data[3440-258+11])
+    _moveablefast.append(data.data[3440-258+25])
+    _moveableslow.append(data.data[3440-258+32])
+    return _movingflags, _powernumbers, _moveablefast, _moveableslow
+
+def extractTargetFromData(data):
+    _grouping_changed = [] 
+    _oncoming =[]
+    _latrate =[]
+    _bridgeflag =[]
+    _width =[]
+    _rangerate =[]
+    _rangeaccel =[]
+    _range =[]
+    _medrangemode =[]
+    _angle = []
+    _status = []
+    for i in range(0,768,12):
+	_range.append(data.data[i+9] * .1)
+	_angle.append(data.data[i+11]* .1)
+	_grouping_changed.append(data.data[i]) 
+	_oncoming.append(data.data[i+1])
+	_latrate.append(data.data[i+2]*.25)
+	_bridgeflag.append(data.data[i+3])
+	_width.append(data.data[i+4]*0.5)
+	_rangerate.append(data.data[i+7]*.01)
+	_rangeaccel.append(data.data[i+8]*.05)
+	_medrangemode.append(data.data[i+10])
+	_status.append(data.data[i+5])
+    return _grouping_changed, _oncoming, _latrate, _bridgeflag, _width, _rangerate, _rangeaccel, _range, _medrangemode, _angle, _status
+
 class RadarVizNode:
     # def startVisuzalizationNode(self,node_name):
     def __init__(self,node_name):
@@ -128,9 +194,9 @@ class RadarVizNode:
 	self.marker.pose.orientation.y = 0.0
 	self.marker.pose.orientation.z = 0.0
 	self.marker.pose.orientation.w = 1.0
-	self.marker.scale.x = 0.10
-	self.marker.scale.y = 0.10
-	self.marker.scale.z = 0.10
+	self.marker.scale.x = 0.50
+	self.marker.scale.y = 0.50
+	self.marker.scale.z = 0.50
 	self.marker.color.r = 0.0
 	self.marker.color.g = 1.0
 	self.marker.color.b = 0.0
@@ -150,36 +216,47 @@ class RadarVizNode:
 	    # self.marker.colors.append( color)
 
     def createPointsInMarker(self, data):
+	track_no_idx = 0
 	for i in range(0,768,12):
+	# if not self.rangerate[track_no_idx] == 81.91:
 	    point = Point()
-	    dist  = data.data[i+9] * .1
-	    angle = data.data[i+11]* .1
+	    dist  = self.dist [track_no_idx] #data.data[i+9] * .1
+	    angle = self.angle[track_no_idx] #data.data[i+11]* .1
 	    angle_rad = math.radians(angle)
 	    point.x = - dist * math.sin(angle_rad)
 	    point.y = - dist * math.cos(angle_rad)
 	    point.z = 0
 	    color = ColorRGBA()
-	    color.r = 0.0
-	    color.g = 1.0
-	    color.b = 0.0
+	    if self.movingflags[track_no_idx]:
+		color.r = 1.0
+		color.g = 0.0
+		color.b = 0.0
+	    else:
+		color.r = 0.0
+		color.g = 1.0
+		color.b = 0.0
 	    color.a = 1.0
 	    self.marker.points.append( point)
 	    self.marker.colors.append( color)
+	    track_no_idx = track_no_idx + 1
 
 
     def createMarkerArray(self):
 	self.marker_array = MarkerArray()
 
     def createTextsInMarkerArray(self, data):
+	track_no_idx = 0
 	for i in range(0,768,12):
+	    # if not self.rangerate[track_no_idx] == 81.91:
 	    dist  = data.data[i+9] * .1
 	    angle = data.data[i+11]* .1
 	    angle_rad = math.radians(angle)
 	    unit = self.spawnTextMarker(i)
-	    unit.text = '+'
+	    unit.text = str(self.powernumbers[track_no_idx] - 10) + ' dB ' + str(self.rangerate[track_no_idx]) + ' ' + str(self.rangeaccel[track_no_idx]) + ' ' + str(self.grouping_changed[track_no_idx]) + ' ' + str(self.width[track_no_idx])
 	    unit.pose.position.x = - dist * math.sin(angle_rad)
-	    unit.pose.position.y = - dist * math.cos(angle_rad)
+	    unit.pose.position.y = - dist * math.cos(angle_rad) + 0.6
 	    self.marker_array.markers.append(unit)
+	    track_no_idx = track_no_idx + 1
 
 
 
@@ -230,7 +307,7 @@ class RadarVizNode:
 	self.marker_text.type =Marker.TEXT_VIEW_FACING
 	self.marker_text.action = Marker.ADD
 	self.marker_text.pose.position.x = 0
-	self.marker_text.pose.position.y = 5
+	self.marker_text.pose.position.y = 1.0
 	self.marker_text.pose.position.z = 0
 	self.marker_text.pose.orientation.x = 0.0
 	self.marker_text.pose.orientation.y = 0.0
@@ -245,6 +322,8 @@ class RadarVizNode:
 	self.marker_text.color.a = 1.0
 
     def onIncomingData(self, msg):
+	self.movingflags, self.powernumbers, self.moveablefast, self.moveableslow = extractTrackingFromData(msg)
+	self.grouping_changed, self.oncoming, self.latrate, self.bridgeflag, self.width, self.rangerate, self.rangeaccel, self.dist, self.medrangemode, self.angle, self.status = extractTargetFromData(msg)
 	self.createCubeMarker()
 	self.createPointsInMarker(msg)
 	self.pub_marker.publish(self.marker)
